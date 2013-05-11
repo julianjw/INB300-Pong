d28fddb79358798ab60af6e08f3b0545062b9108
Julian <julianjw5@hotmail.com>
2013-05-11 13:04:18 +1000

    Initial Commit of Pong

---
+++ Pong/KinectPong-master/Pong/Pong/PongGame.cs
@@ -0,0 +1,418 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+using Microsoft.Kinect;
+using Coding4Fun.Kinect.WinForm;
+
+using System.IO;
+using System.Windows;
+using System.Windows.Media;
+
+//using Microsoft.Kinect.Toolkit;
+
+namespace Pong
+{
+    internal static class SkeletalCommonExtensions
+    {
+        public static Joint ScaleTo(this Joint joint, int width, int height, float skeletonMaxX, float skeletonMaxY)
+        {
+            Microsoft.Kinect.SkeletonPoint pos = new SkeletonPoint()
+            {
+                X = Scale(width, skeletonMaxX, joint.Position.X),
+                Y = Scale(height, skeletonMaxY, -joint.Position.Y), //-joint.Position.Y
+                Z = joint.Position.Z
+            };
+
+            joint.Position = pos;
+
+            return joint;
+        }
+
+        public static Joint ScaleTo(this Joint joint, int width, int height)
+        {
+            return ScaleTo(joint, width, height, 1.0f, 1.0f);
+        }
+
+        private static float Scale(int maxPixel, float maxSkeleton, float position)
+        {
+            //position = (float)((((0.5 / 0.25f) / 2) * position) + (0.5 / 2));
+            //if (position > 0.5f)
+            //    position = 0.5f;
+            //if (position < 0)
+            //    position = 0;
+
+            //float value = ((((maxPixel / maxSkeleton) / 2) * position) + (maxPixel / 2));
+            //if (value > maxPixel)
+            //    return maxPixel;
+            //if (value < 0)
+            //    return 0;
+            //return value;
+
+            //Make the sure the position is positive so the hand position is being used properly
+            position = Math.Abs(position);
+
+            //Make sure the hand position is within the boundaries of the 0.5 - 0.0 scaled set
+            if (position > 0.5f)
+                return 0;
+            if (position < 0)
+                return maxPixel;
+
+            //Get the percentage of the hand position within the 0.5 - 0.0 scaled set
+            float percent = position / maxSkeleton; 
+
+            //Get the new position of the bat using the scaled hand position
+            int value = (int)(maxPixel - (maxPixel * percent));
+
+            //Return the new bat position
+            return value;
+        }
+    }
+
+
+	public class PongGame : Microsoft.Xna.Framework.Game
+	{
+
+		const int kLRMargin = 20, kPaddleWidth = 26, kPaddleHeight = 120;
+		const int kBallWidth = 24, kBallHeight = 24;
+		const int kMaxAIPaddleVelocity = 7;
+		const int kGameWidth = 1360, kGameHeight = 800;
+		
+		bool passedCenter = false;
+		
+		GraphicsDeviceManager graphics;
+		SpriteBatch spriteBatch;
+		
+		Texture2D dotTexture = null, ballTexture = null;
+		
+		Rectangle ourPaddleRect = new Rectangle(kLRMargin, 0, kPaddleWidth, kPaddleHeight);
+		Rectangle aiPaddleRect;
+		
+		Vector2 ballVelocity;
+		Rectangle ballRect;
+		
+		float predictedBallHeight = 0.0f;
+
+        int player1Score = 0;
+        int player2Score = 0;
+
+        SpriteFont gameFont;
+
+        float handPos;
+        int ballVelocityX;
+        int ballVelocityY;
+
+        string gameText = "";
+
+        DateTime time = new DateTime();
+
+		
+		//KinectSensor runtime = new KinectSensor();
+
+        //private Skeleton[] skeletonData;
+
+        /// <summary>
+        /// Active Kinect sensor
+        /// </summary>
+        private KinectSensor sensor;
+
+		public PongGame()
+		{
+			graphics = new GraphicsDeviceManager(this);
+			graphics.PreferredBackBufferWidth = kGameWidth;
+			graphics.PreferredBackBufferHeight = kGameHeight;
+			
+			Content.RootDirectory = "Content";
+		}
+		
+		private void RestartGame()
+		{
+            aiPaddleRect = new Rectangle(GraphicsDevice.Viewport.Width - kLRMargin - kPaddleWidth, 20, kPaddleWidth, kPaddleHeight);
+            ballRect = new Rectangle(500, 600, kBallWidth, kBallHeight);
+
+            if (player1Score >= 3)
+            {
+                //set text to player 1 wins
+                gameText = "Player 1 Wins!";
+                ballVelocity = new Vector2(0.0f, 0.0f);
+            }
+            else if (player2Score >= 3)
+            {
+                //set text to player 2 wins
+                gameText = "Player 2 Wins!";
+                ballVelocity = new Vector2(0.0f, 0.0f);
+            }
+            else
+            {
+                ballVelocity = new Vector2((float)new Random(time.Millisecond).Next(-10, 10), (float)new Random(time.Millisecond).Next(-10, 10));
+                while (ballVelocity.X == 0 || (ballVelocity.X >= -6 && ballVelocity.X <= 0) || (ballVelocity.X <= 6 && ballVelocity.X >= 0)) {
+                    ballVelocity.X = new Random().Next(-10, 10);
+                }
+                while (ballVelocity.Y == 0 || (ballVelocity.Y >= -6 && ballVelocity.Y <= 0) || (ballVelocity.Y <= 6 && ballVelocity.Y >= 0))
+                {
+                    ballVelocity.Y = new Random().Next(-10, 10);
+                }
+                ballVelocityX = (int)ballVelocity.X;
+                ballVelocityY = (int)ballVelocity.Y;
+            }
+
+		}
+		
+		protected override void Initialize()
+		{
+			RestartGame();
+
+            foreach (var potentialSensor in KinectSensor.KinectSensors)
+            {
+                if (potentialSensor.Status == KinectStatus.Connected)
+                {
+                    this.sensor = potentialSensor;
+                    break;
+                }
+            }
+
+            if (null != this.sensor)
+            {
+                // Turn on the skeleton stream to receive skeleton frames
+                this.sensor.SkeletonStream.Enable();
+
+                // Add an event handler to be called whenever there is new color frame data
+                this.sensor.SkeletonFrameReady += this.SensorSkeletonFrameReady;
+
+                // Start the sensor!
+                try
+                {
+                    this.sensor.Start();
+                }
+                catch (IOException)
+                {
+                    this.sensor = null;
+                }
+            }
+			
+			base.Initialize();
+		}
+
+
+
+        private void SensorSkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+        {
+            Skeleton[] skeletons = new Skeleton[0];
+
+            using (SkeletonFrame skeletonFrame = e.OpenSkeletonFrame())
+            {
+                if (skeletonFrame != null)
+                {
+                    skeletons = new Skeleton[skeletonFrame.SkeletonArrayLength];
+                    skeletonFrame.CopySkeletonDataTo(skeletons);
+                }
+            }
+
+            if (skeletons.Length != 0)
+                {
+                foreach (Skeleton skel in skeletons)
+                {
+                    if (SkeletonTrackingState.Tracked == skel.TrackingState)
+                    {
+                        Joint joint = skel.Joints[JointType.HandRight];
+
+                        int handY = (int)SkeletalCommonExtensions.ScaleTo(joint, kGameWidth, kGameHeight - kPaddleHeight, 0.5f, 0.5f).Position.Y;
+
+                        handPos = joint.Position.Y;
+
+                        ourPaddleRect.Y = handY;
+
+                        break;
+                    }
+                }
+            }
+        }
+
+		protected override void LoadContent()
+		{
+			spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            gameFont = Content.Load<SpriteFont>("Scoreboard");
+
+			dotTexture = Content.Load<Texture2D>("Dot");
+			ballTexture = Content.Load<Texture2D>("Ball");
+		}
+		
+		protected override void UnloadContent()
+		{
+            if (this.sensor != null)
+            {
+                this.sensor.Stop();
+            }
+		}
+		
+		private void SimulateRestOfTurn()
+		{
+			Rectangle currentBallRect = ballRect;
+			Vector2 currentBallVelocity = ballVelocity;
+			
+			bool done = false;
+			
+			while (!done)
+			{
+				BallCollision result = AdjustBallPositionWithScreenBounds(ref currentBallRect, ref currentBallVelocity);
+				done = (result == BallCollision.RightMiss || result == BallCollision.RightPaddle);
+                //spriteBatch.DrawString(gameFont, "Player 1: " + player1Score + " | Player 2: " + player2Score);
+
+
+			}
+			
+			predictedBallHeight = currentBallRect.Y;
+		}
+		
+		enum BallCollision
+		{
+			None,
+			RightPaddle,
+			LeftPaddle,
+			RightMiss,
+			LeftMiss
+		}
+		
+		private BallCollision AdjustBallPositionWithScreenBounds(ref Rectangle enclosingRect, ref Vector2 velocity)
+		{
+			BallCollision collision = BallCollision.None;
+			
+			enclosingRect.X += (int)velocity.X;
+			enclosingRect.Y += (int)velocity.Y;
+			
+			if (enclosingRect.Y >= GraphicsDevice.Viewport.Height - kBallHeight)
+			{
+				velocity.Y *= -1;
+			}
+			else if (enclosingRect.Y <= 0)
+			{
+				velocity.Y *= -1;
+			}
+			
+			if (aiPaddleRect.Intersects(enclosingRect))
+			{
+				velocity.X *= -1;
+				collision = BallCollision.RightPaddle;
+			}
+			else if (ourPaddleRect.Intersects(enclosingRect))
+			{
+				velocity.X *= -1;
+				collision = BallCollision.LeftPaddle;
+			}
+			else if (enclosingRect.X >= GraphicsDevice.Viewport.Width - kBallWidth)
+			{
+				collision = BallCollision.RightMiss;
+			}
+			else if (enclosingRect.X <= 0)
+			{
+				collision = BallCollision.LeftMiss;
+			}
+			
+			return collision;
+		}
+		
+		protected override void Update(GameTime gameTime)
+		{
+			if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+				this.Exit();
+
+            if (Keyboard.GetState(PlayerIndex.One).IsKeyDown(Keys.Escape))
+                this.Exit();
+
+            if (Keyboard.GetState(PlayerIndex.One).IsKeyDown(Keys.N))
+            {
+                player1Score = 0;
+                player2Score = 0;
+                gameText = "";
+                RestartGame();
+            }
+
+            
+			
+			BallCollision collision = AdjustBallPositionWithScreenBounds(ref ballRect, ref ballVelocity);
+			
+			if (collision > 0)
+			{
+				passedCenter = false;
+				
+				float newY = (new Random().Next(80) + 1) / 10.0f;
+				ballVelocity.Y = ballVelocity.Y > 0 ? newY : -newY;
+			}
+			
+			if (collision == BallCollision.RightMiss || collision == BallCollision.LeftMiss)
+			{
+                //Changes the score to reflect who won the point (who missed the ball)
+                if (BallCollision.RightMiss == collision) {
+                    player1Score += 1;
+                } else if (BallCollision.LeftMiss == collision) {
+                    player2Score += 1;
+                }
+
+				RestartGame();
+			}
+			
+			if (passedCenter == false && ballVelocity.X > 0 && (ballRect.X + kBallWidth >= GraphicsDevice.Viewport.Bounds.Center.X))
+			{
+				SimulateRestOfTurn();
+				passedCenter = true;
+			}
+			
+			int ballCenter = (int)predictedBallHeight + (kBallHeight / 2);
+			int aiPaddleCenter = aiPaddleRect.Center.Y;
+			
+			if (predictedBallHeight > 0 && ballCenter != aiPaddleCenter)
+			{
+				if (ballCenter < aiPaddleCenter)
+				{
+					aiPaddleRect.Y -= kMaxAIPaddleVelocity;
+				}
+				else if (ballCenter > aiPaddleCenter)
+				{
+					aiPaddleRect.Y += kMaxAIPaddleVelocity;
+				}
+				
+				if (Math.Abs(ballCenter - aiPaddleCenter) < kMaxAIPaddleVelocity)
+				{
+					aiPaddleRect.Y = ballCenter - (kPaddleHeight / 2);
+				}
+			}
+			
+			base.Update(gameTime);
+		}
+		
+		protected override void Draw(GameTime gameTime)
+		{
+			GraphicsDevice.Clear(Color.White);
+			
+			spriteBatch.Begin();
+			
+			spriteBatch.Draw(dotTexture, ourPaddleRect, Color.Red);
+			spriteBatch.Draw(dotTexture, aiPaddleRect, Color.IndianRed);
+			spriteBatch.Draw(ballTexture, ballRect, Color.Red);
+
+            Vector2 position = new Vector2(500.0f, 10.0f);
+            Vector2 position2 = new Vector2(500.0f, 30.0f);
+            Vector2 position3 = new Vector2(500.0f, 300.0f);
+            Vector2 position4 = new Vector2(500.0f, 50.0f);
+            Vector2 position5 = new Vector2(500.0f, 70.0f);
+
+            spriteBatch.DrawString(gameFont, (player1Score + " | " + player2Score), position, Color.Black);
+            spriteBatch.DrawString(gameFont, ("Hand Position on Screen: " + handPos),position2,Color.Black);
+            spriteBatch.DrawString(gameFont, ("Ball Velocity X: " + ballVelocityX), position4, Color.Black);
+            spriteBatch.DrawString(gameFont, ("Ball Velocity Y: " + ballVelocityY), position5, Color.Black);
+            spriteBatch.DrawString(gameFont, gameText, position3, Color.SteelBlue);
+			
+			spriteBatch.End();
+			
+			base.Draw(gameTime);
+		}
+	}
+}
